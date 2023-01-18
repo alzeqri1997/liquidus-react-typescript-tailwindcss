a287a46ff559339492d5bfc64d51dd884b1ec4b1
alzeqri1997 <ahmed1221moh@gmail.com>
2023-01-18 22:30:07 +0300

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: '#17E7D6',
+      white: '#ffffff',
+      dark: '#040618'
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '11.625rem',
+      },
+    }
+  },
+  plugins: [],
+}
+
